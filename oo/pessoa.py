abb88e46f9ca5a7d7de4f36c66fc3fdad5136dfa
Mauricio Doerr <mauricio.doerr@gmail.com>
2023-01-15 09:44:14 -0300

    Criado método cumprimentar para classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,20 @@
 class Pessoa:
-    pass
+    def __init__(self, nome: str = None, idade: int = 0):
+        self.nome = nome
+        self.idade = idade
+
+    def cumprimentar(self):
+        return f'Olá {self.nome}, você possui {self.idade} anos de idade.'
+
+
+if __name__ == '__main__':
+    p = Pessoa('Teste')
+    print(p.cumprimentar())
+    p.nome = 'Teste2'
+    print(p.cumprimentar())
+
+    p2 = Pessoa('TesteIdade', 10)
+    print(p2.cumprimentar())
+
+    p3 = Pessoa(idade=30)
+    print(p3.cumprimentar())
